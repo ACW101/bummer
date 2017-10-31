3e2fd227dae482d4bc989f143237613babdeda4a
travis <travischan@foxmail.com>
2017-10-31 19:42:31 -0400

    BottonNavi1

---
+++ app/src/main/java/edu/brandeis/cs/bummer/PostActivity/PostActivity.java
@@ -0,0 +1,8 @@
+package edu.brandeis.cs.bummer.PostActivity;
+
+/**
+ * Created by yancheng on 2017/10/31.
+ */
+
+public class PostActivity {
+}
--- app/src/main/java/edu/brandeis/cs/bummer/Profile/ProfileActivity.java
+++ app/src/main/java/edu/brandeis/cs/bummer/Profile/ProfileActivity.java
@@ -1,8 +1,17 @@
 package edu.brandeis.cs.bummer.Profile;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by yancheng on 2017/10/31.
  */
 
-public class ProfileActivity {
+public class ProfileActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
