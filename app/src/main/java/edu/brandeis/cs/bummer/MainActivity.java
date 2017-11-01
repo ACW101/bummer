641291195c8551554981c76f80223f8bb139db50
Kevin Wang <kevinwang101@gmail.com>
2017-11-01 14:52:18 -0400

    Implement authentication

--- app/src/main/java/edu/brandeis/cs/bummer/MainActivity.java
+++ app/src/main/java/edu/brandeis/cs/bummer/MainActivity.java
@@ -1,13 +1,53 @@
 package edu.brandeis.cs.bummer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private FirebaseUser currentUser;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Buttons
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        this.currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.d(TAG, "current user = " + currentUser.getUid());
+        } else {
+
+        }
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        startActivity(new Intent(this, EmailPasswordActivity.class));
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_out_button) {
+            signOut();
+        }
     }
 }
