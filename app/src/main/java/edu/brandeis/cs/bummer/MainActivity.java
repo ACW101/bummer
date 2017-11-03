1fae79c303ccbd8083fc4cc4bf2c1ee6691eca21
JackyWang <jackywang@git.com>
2017-11-02 21:39:13 -0400

    map added

--- app/src/main/java/edu/brandeis/cs/bummer/MainActivity.java
+++ app/src/main/java/edu/brandeis/cs/bummer/MainActivity.java
@@ -1,11 +1,12 @@
 package edu.brandeis.cs.bummer;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -13,19 +14,21 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import edu.brandeis.cs.bummer.Utils.BottomNavigationHelper;
 
 public class MainActivity extends AppCompatActivity {
+
     private Context mContext = MainActivity.this;
     private static final int ACTIVITY_NUM = 0;
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = findViewById(R.id.ImageView);
         imageView.setImageResource(R.drawable.jp_monkey);
         setupBottomNavigationView();
     }
+
+
     private void setupBottomNavigationView(){
 
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
@@ -36,5 +39,12 @@ public class MainActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+
+    public void MapsHome(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 
 }
